c98ffd0075702f6c83f24f9e3814679a6a8f5fcd
Alessandro Candido <candido.ale@gmail.com>
2023-06-03 21:10:48 +0200

    Install rich traceback, to prettify errrors as well

--- src/qibodb/cli/log.py
+++ src/qibodb/cli/log.py
@@ -3,6 +3,7 @@ import logging
 
 from rich.console import Console
 from rich.logging import RichHandler
+from rich.traceback import install
 
 logging.basicConfig(
     level=logging.INFO,
@@ -10,3 +11,4 @@ logging.basicConfig(
     datefmt="[%X]",
     handlers=[RichHandler(console=Console(stderr=True))],
 )
+install()
